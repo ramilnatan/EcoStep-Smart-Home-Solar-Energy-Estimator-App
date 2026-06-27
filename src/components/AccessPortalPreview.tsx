e4cc5acff743c9cd4031897d35e3023fb2ee848d
Ramil Natan <ramil_natan@yahoo.com>
2026-06-27 21:53:31 +0300

    Add Access Portal navigation link

--- src/components/AccessPortalPreview.tsx
+++ src/components/AccessPortalPreview.tsx
@@ -14,7 +14,7 @@ export function AccessPortalPreview() {
   };
 
   return (
-    <section className="relative bg-dark-900 py-24">
+    <section id="access-portal" className="relative bg-dark-900 py-24">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <motion.div
           initial={{ opacity: 0, y: 24 }}
